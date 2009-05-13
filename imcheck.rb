0df5b86ce8c1c0dc95b9fb8fa563a6aa3640c88c
Fabian Franke <github@fabianfranke.de>
2009-05-13 11:33:33 +0200

    extended plan comments

--- imcheck.rb
+++ imcheck.rb
@@ -1,11 +1,20 @@
 # einloggen
+# => http://www.imperaonline.de/login.php
+# =>  post
+# =>  username -> uname
+# =>  passwort -> upass
 # session speichern (?)
+# =>  cookie speichern -> PHPSESSID
 # games seite aufrufen
+# => http://www.imperaonline.de/game/games
 # html durchparsen
 # spielart, spielid, wer am zug ist auslesen
+# => bodytoppan -> h3 (loop) -> tbody (loop) -> loop über die tds
 # ausloggen
+# => http://www.imperaonline.de/extern/logout.php
 # jabber login
 # über spielernamen loopen
 # falls jabberaccount für spielername besteht msg
 # jabber logout
 # cron 2 min
+
