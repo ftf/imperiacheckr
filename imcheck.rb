542a390be8409fede769b7cda8c87f54d8f5a1ae
Fabian Franke <mail@fabianfranke.de>
2009-05-13 10:55:30 +0200

    initial commit, just comments

---
+++ imcheck.rb
@@ -0,0 +1,11 @@
+# einloggen
+# session speichern (?)
+# games seite aufrufen
+# html durchparsen
+# spielart, spielid, wer am zug ist auslesen
+# ausloggen
+# jabber login
+# über spielernamen loopen
+# falls jabberaccount für spielername besteht msg
+# jabber logout
+# cron 2 min
